048410b7f47d07efa6a6991b06018ea3ff295328
Mike <mike.blackburn.541@gmail.com>
2023-01-23 17:27:30 -0500

    first commit

---
+++ Circle Objects Lab/Circle.cs
@@ -0,0 +1,58 @@
+﻿namespace Circle_Objects_Lab
+{
+  public class Circle
+  {
+    //properties
+    public double radius { get; set; }
+    public double Circumference { get; set; }
+    public double Area { get; set; }
+
+    //Constructor
+    public Circle(double _radius)
+    {
+      radius = _radius;
+      Circumference = CalculateCircumference(_radius);
+      Area = CalculateArea(_radius);
+    }
+
+
+    public Circle()    //Overloaded = default
+    {
+      radius = 5;
+      Circumference = CalculateCircumference(5);
+      Area = CalculateArea(5);
+
+    }
+
+    //methods
+    //public double CalculateCircumference()
+    public double CalculateCircumference(double radius)
+    {
+      Circumference = 2 * (Math.PI * radius);
+      return Circumference;
+    }
+    //public string CalculateFormattedCircumference()
+    public string CalculateFormattedCircumference(double x)
+    {
+      Circumference = 2 * (Math.PI * radius);
+      return Math.Round(Circumference, 2).ToString();
+    }
+
+
+    //public double CalculateArea()
+    public double CalculateArea(double radius)
+    {
+      Area = Math.PI * (Math.Pow(radius, 2));
+      return Area;
+    }
+    //public string CalculateFormattedArea()
+    public string CalculateFormattedArea(double x)
+    {
+      Area = Math.PI * (Math.Pow(radius, 2));
+      return Math.Round(Area, 2).ToString();
+    }
+  //private string FormatNumber(double x)
+
+
+}
+}
---
+++ Circle Objects Lab/Program.cs
@@ -0,0 +1,101 @@
+﻿using Circle_Objects_Lab;
+
+
+// Task: Calculate a circle’s circumference and area.
+//
+///
+// What will the application do?
+
+//*   The application prompts the user to enter a radius; the user may enter a decimal number.
+
+//*   The application displays an error if the user enters invalid data and asks the user again for a radius.
+
+//*   When the user enters valid data, the application calculates the area and circumference of the circle and
+//rounds to the nearest 2 decimal places and prints out both.
+
+//*   The application asks the user whether they want to do another.
+
+//*   The application displays a “goodbye” message that also indicates the number of circles the user built when
+//the user chooses not to continue.
+///
+// Build Specifications:
+//
+///
+// 1) Create a class named Circle to store the data about this circle. This class should contain these constructors and methods:
+//    a) public Circle(double radius)
+
+//    b) public double CalculateCircumference()
+
+//    c) public string CalculateFormattedCircumference()
+
+//    d) public double CalculateArea()
+
+//    e) public string CalculateFormattedArea()
+
+//    f) private string FormatNumber(double x)
+///g) h)     These two are optional
+///    g) Define a member called radius. This member should be private. 
+///    h) Define a property to get access to the class member: Radius 
+// 2) For the value of pi, use the PI constant of the System.Math class.
+//
+///
+// 3) Get the user input, create a Circle object, and display the circumference and area.
+//
+
+///Hints
+///   Don’t mess up the formulas for circumference or area of a circle!
+//
+/// Extra Challenges:
+///    a) Create a class named Validator and use its static methods to validate the data in this application.
+
+
+Console.WriteLine("Welcome to the Circle Tester\n");
+//loop program
+bool runProgram = true;
+int Count = 1;
+while (runProgram)
+{
+  Console.Write("Enter radius: ");
+  double radius = double.Parse(Console.ReadLine());
+
+  Circle Circles = new Circle(radius);
+  Circles.CalculateCircumference(radius);
+  Console.WriteLine($"Circumfrence: {Circles.CalculateFormattedCircumference(radius)}");
+  Console.WriteLine($"Area: {Circles.CalculateFormattedArea(radius)}");
+
+  while (runProgram = true)
+  {
+    Console.WriteLine("Continue? (y/n)");
+    string answer = Console.ReadLine();
+
+    if (answer == "y")
+    {
+      Console.WriteLine("Cool.");
+      runProgram = true;
+      Count = Count + 1;
+      Console.Clear();
+      break;
+    }
+    else if (answer == "n")
+    {
+      runProgram = false;
+      if (Count < 2)
+      {
+        Console.WriteLine($"You made {Count} circle object.");
+        Console.WriteLine("Goodbye!");
+        break;
+      }
+      else
+      {
+        Console.WriteLine($"You made {Count} circle objects.");
+        Console.WriteLine("Goodbye!");
+        break;
+      }
+    }
+    else
+    {
+      Console.WriteLine($"{answer} is not a valid selection, please try again.");
+    }
+  }
+}
+
